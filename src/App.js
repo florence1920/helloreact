056832a9142eab880415b04ae1dc6bc176e89598
lsh <jetaime12121@gmail.com>
2023-05-25 22:50:14 +0900

    event function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import MyComponent from './MyComponent';
 import Say from './Say';
 import EventPractice from './EventPractive'
+import EventFunction from './EventFunction'
 
 const App = () =>{
-  return <EventPractice/>
+  return <EventFunction/>
 }
 
 export default App;
